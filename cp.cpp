fe6d7afb5c0509849a2e7a5f298e2018d27414b7
Nabeel <nabeelismaeel74@gmail.com>
2023-12-12 20:17:53 +0300

    test

---
+++ cp.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#define fastIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define fileIO freopen("input.in", "r", stdin); freopen("output.in", "w", stdout);
+#define all(x) (x).begin(),(x).end()
+#define endl "\n" ; 
+typedef long long ll;
+using namespace std ;
+
+
+
+
+
+int  main() {
+    fastIO   
+
+    cout<<"hello world" ;
+
+    return 0 ;  
+}
